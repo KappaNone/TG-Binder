886f2f5909ad50334aec15cf1b5c006d18c7ecda
KappaNone <121200875+KappaNone@users.noreply.github.com>
2023-11-01 10:54:38 +0200

    Pretty code :>

--- core.js
+++ core.js
@@ -9,9 +9,9 @@ const stringSession = new StringSession(process.env.STRING_SESSION);
 
 
 //* The channel from which the content will be taken
-const sourceChannelId = process.env.SOURCE_CHANNEL_ID;
+const sourceChannelId = "ENTER_YOUR_VALUE";
 //* The channel where the content will be redirected to
-const finalChannelId = process.env.FINAL_CHANNEL_ID;
+const finalChannelId = "ENTER_YOUR_VALUE";
 
 (async () => {
   console.log("Loading...");
@@ -21,18 +21,18 @@ const finalChannelId = process.env.FINAL_CHANNEL_ID;
   await client.start(
 
     //* Authorization in the client
+    // {
+    //   phoneNumber: async () => await input.text("Please enter your phone number: "),
+    //   password: async () => await input.text("Please enter your password: "),
+    //   phoneCode: async () =>
+    //     await input.text("Please enter the code you received: "),
+    //   onError: (err) => console.log(err),
+    // }
 
-    {
-      phoneNumber: async () => await input.text("Please enter your phone number: "),
-      password: async () => await input.text("Please enter your password: "),
-      phoneCode: async () =>
-        await input.text("Please enter the code you received: "),
-      onError: (err) => console.log(err),
-    }
   );
 
   //* Get your stringSession
-  console.log(client.session.save())
+  // console.log(client.session.save())
   
   console.log("You should now be connected.");
 
@@ -51,29 +51,9 @@ const finalChannelId = process.env.FINAL_CHANNEL_ID;
       let msg = event.message.message;
       console.log(msg);
 
-      const firstCodePoint = msg.codePointAt(0);
-
-      const isEmojiCharacter = isEmojiCodePoint(firstCodePoint);
-      if (isEmojiCharacter) {
-        await client.sendMessage(finalChannelId, { message: msg });
-      }
+      await client.sendMessage(finalChannelId, { message: msg });
     }
 
   });
-
-  function isEmojiCodePoint(codePoint) {
-    return (
-      (codePoint >= 0x1F300 && codePoint <= 0x1F5FF) ||
-      (codePoint >= 0x1F600 && codePoint <= 0x1F64F) ||
-      (codePoint >= 0x1F680 && codePoint <= 0x1F6FF) ||
-      (codePoint >= 0x2600 && codePoint <= 0x26FF) ||
-      (codePoint >= 0x2700 && codePoint <= 0x27BF) ||
-      (codePoint >= 0x1F900 && codePoint <= 0x1F9FF) ||
-      (codePoint >= 0x1F1E6 && codePoint <= 0x1F1FF)
-    );
-  }
-
-
-
 })();
 
